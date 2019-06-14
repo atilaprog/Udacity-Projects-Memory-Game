0ac606c73e3df9a51763f1eff0d339fab6f14368
atilaprog <48094246+atilaprog@users.noreply.github.com>
2019-06-14 16:16:03 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -9,13 +9,23 @@ const stars = document.querySelector('.stars')
 const listOfStars = document.querySelectorAll('.li-stars')
 const starChild = undefined
 const increaseMoves = document.querySelector('.moves')
+let hoursTimer = document.querySelector('.hours')
+let minutesTimer1 = document.querySelector('.minutes1')
+let secondsTimer1 = document.querySelector('.seconds1')
+let minutesTimer2 = document.querySelector('.minutes2')
+let secondsTimer2 = document.querySelector('.seconds2')
+let secondsTimer3 = document.querySelector('.seconds3')
 
 /*-----Arrays-----*/
 let cardsToShuffle = [].slice.call(allCards.children)
 /*-----Deck Match-----*/
 let deckMatch = []
+/*-----Deck to loose stars-----*/
+let deckChances = []
 /*-----Deck Opened-----*/
 let deckOpened = []
+/*-----Deck Check Timer-----*/
+let deckTimer = []
 /*-----Array to shuffle-----*/
 let classToShuffle = ['diamond', 'diamond', 'paper-plane-o', 'paper-plane-o', 'anchor', 'anchor', 'bolt', 
 'bolt', 'cube', 'cube', 'leaf', 'leaf', 'bicycle', 'bicycle', 
@@ -27,8 +37,61 @@ allCards.addEventListener('click', addOpenShowClass)
 resetButton.addEventListener('click', resetGame)
 
 /*-----Functions-----*/
+//timer function
+function timer(){
+    
+    setInterval(() => {
+        secondsTimer3.textContent++
+        if(secondsTimer3.textContent == 10){
+            secondsTimer3.textContent = 0
+        }
+    }, 100);
+
+    setInterval(() => {
+        secondsTimer2.textContent++
+        if(secondsTimer2.textContent == 10){
+            secondsTimer2.textContent = 0
+        }
+    }, 1000);
+
+    setInterval(() => {
+        secondsTimer1.textContent++
+        if(secondsTimer1.textContent == 6){
+            secondsTimer1.textContent = 0
+        }
+    }, 10000);
+
+    setInterval(() => {
+        minutesTimer2.textContent++
+        if(minutesTimer2.textContent == 9){
+            minutesTimer2.textContent = 0
+        }
+    }, 60000);
+
+    setInterval(() => {
+        minutesTimer2.textContent++
+        if(minutesTimer2.textContent == 6){
+            minutesTimer2.textContent = 0
+        }
+    }, 600000);
+
+    setInterval(() => {
+        hoursTimer.textContent++
+        if(hoursTimer.textContent == 24){
+            hoursTimer.textContent = 0
+        }
+    }, 3600000);
+    
+    
+    
+}
 //function to add open and match class in target
-function addOpenShowClass (event){ 
+function addOpenShowClass (event){
+
+    if(deckTimer.length == 0){
+        deckTimer.push('check')
+        timer()
+    }
     
     let target = event.target
     let child = target.firstElementChild
@@ -56,13 +119,13 @@ function addOpenShowClass (event){
 //function if you loose a chance
 function lostChances (){
     
-    let starChild = document.querySelector('.fa-star')
+    if(deckChances.length == 3 || deckChances.length == 6 || deckChances.length == 9 ){ let starChild = document.querySelector('.fa-star')
     starChild.classList.remove('fa-star')
     starChild.classList.add('fa-star-o', '1')
     
-    if(listOfStars[6].classList.contains('1')){
+    if(listOfStars[2].classList.contains('1')){
         resetGame()
-    }
+    }}
 }
 
 
@@ -115,6 +178,8 @@ function checkMatch(){
             closeCard(firstClick.parentElement)
             closeCard(lastClick.parentElement)
             deckOpened.length = 0
+            deckChances.push('check')
+
             lostChances()
         }
     }
